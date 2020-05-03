21a5892f5845456290e33d723285fa516508f13d
Antop <antop@kakao.com>
2020-05-03 17:41:23 +0900

    algorithm-70 70. Climbing Stairs

---
+++ src/main/kotlin/com/leetcode/P70.kt
@@ -0,0 +1,19 @@
+package com.leetcode
+
+// https://leetcode.com/problems/climbing-stairs/
+class P70 {
+    fun climbStairs(n: Int): Int {
+        if (n == 1) return 1
+
+        val dp = Array(n + 1) { 0 }.apply {
+            this[1] = 1
+            this[2] = 2
+        }
+
+        for (i in 3..n) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n]
+    }
+}
---
+++ src/test/kotlin/com/leetcode/P70Test.kt
@@ -0,0 +1,34 @@
+package com.leetcode
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.jupiter.api.Test
+
+internal class P70Test {
+    val p = P70()
+
+    @Test
+    fun `example 1`() {
+        assertThat(p.climbStairs(2), `is`(2))
+    }
+
+    @Test
+    fun `example 2`() {
+        assertThat(p.climbStairs(3), `is`(3))
+    }
+
+    @Test
+    fun `example 3`() {
+        assertThat(p.climbStairs(4), `is`(5))
+    }
+
+    @Test
+    fun `example 4`() {
+        assertThat(p.climbStairs(5), `is`(8))
+    }
+
+    @Test
+    fun `example 5`() {
+        assertThat(p.climbStairs(6), `is`(13))
+    }
+}
