9d73804df8dd031465e80b5f01cfb174e2fe160a
Antop <antop@kakao.com>
2020-05-13 23:27:31 +0900

    실력이 늘었다.

--- src/main/kotlin/com/leetcode/P20.kt
+++ src/main/kotlin/com/leetcode/P20.kt
@@ -3,42 +3,20 @@ package com.leetcode
 // https://leetcode.com/problems/valid-parentheses/
 class P20 {
     fun isValid(s: String): Boolean {
-        // 닫혀야 하는 문자의 개수
-        val close = s.filter { c -> c == ')' || c == '}' || c == ']' }.count()
-        // 닫힌 문제 개수
-        var closed = 0
-        //  스택
         val stack = mutableListOf<Char>()
-
         for (c in s) {
             when (c) {
                 // 여는 문자를 만나면 스택에 넣기
                 '(', '[', '{' -> stack.push(c)
                 // 닫는 문자를 만났을 때 판단
-                ')' -> {
-                    if (stack.peek() == '(') {
-                        stack.pop()
-                        closed++
-                    }
-                }
-                ']' -> {
-                    if (stack.peek() == '[') {
-                        stack.pop()
-                        closed++
-                    }
-                }
-                '}' -> {
-                    if (stack.peek() == '{') {
-                        stack.pop()
-                        closed++
-                    }
-                }
+                ')' -> if (stack.pop() != '(') return false
+                ']' -> if (stack.pop() != '[') return false
+                '}' -> if (stack.pop() != '{') return false
+                else -> return false;
             }
         }
-        return close == closed && stack.isEmpty()
+        return stack.isEmpty()
     }
-
-
 }
 
 // https://stackoverflow.com/a/60010142/13122945
