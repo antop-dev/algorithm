c1241e901af2404dd26343a35c77c879d9b8627c
Antop <antop@kakao.com>
2021-04-12 02:05:38 +0900

    algorithm-233 멀쩡한 사각형

--- src/main/kotlin/kr/co/programmers/P62048.kt
+++ src/main/kotlin/kr/co/programmers/P62048.kt
@@ -2,20 +2,14 @@ package kr.co.programmers
 
 // https://programmers.co.kr/learn/courses/30/lessons/62048
 class P62048 {
+
     fun solution(w: Int, h: Int): Long {
-        return (w * h).toLong() - (w + h - gcd(w, h))
+        return (w.toLong() * h.toLong()) - (w + h - gcd(w, h))
     }
 
     // greatest common factor
-    private fun gcd(x: Int, y: Int): Int {
-        var a = maxOf(x, y)
-        var b = minOf(x, y)
-        while (b > 0) {
-            val r = a % b
-            a = b
-            b = r % b
-        }
-        return a
+    private fun gcd(n1: Int, n2: Int): Int {
+        return if (n2 != 0) gcd(n2, n1 % n2) else n1
     }
 
 }
