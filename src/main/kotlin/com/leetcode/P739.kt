3d6703d2449b7ccba7c168490fa5daa4c37d42e5
Antop <antop@kakao.com>
2021-05-23 14:53:01 +0900

    algorithm-125 739. Daily Temperatures

--- src/main/kotlin/com/leetcode/P739.kt
+++ src/main/kotlin/com/leetcode/P739.kt
@@ -1,20 +1,20 @@
 package com.leetcode
 
-// https://leetcode.com/problems/daily-temperatures/
+import java.util.*
+
+// https://github.com/antop-dev/algorithm/issues/125
 class P739 {
     fun dailyTemperatures(temperatures: IntArray): IntArray {
-        val size = temperatures.size
-        val tmp = IntArray(size)
-        val answer = IntArray(size)
-        for (i in 1 until size) {
-            if (temperatures[i - 1] < temperatures[i]) {
-                for (j in i - 1 downTo 0) {
-                    if (tmp[j] > -1 && temperatures[j] < temperatures[i]) {
-                        tmp[j] = -1
-                        answer[j] = i - j
-                    }
-                }
+        val answer = IntArray(temperatures.size)
+        val stack = Stack<Int>().apply { push(temperatures.lastIndex) }
+        for (i in temperatures.lastIndex - 1 downTo 0) {
+            while (stack.isNotEmpty() && temperatures[i] >= temperatures[stack.peek()]) {
+                stack.pop()
             }
+            if (stack.isNotEmpty()) {
+                answer[i] = stack.peek() - i
+            }
+            stack.push(i)
         }
         return answer
     }
