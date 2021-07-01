1468987206896e51c08964e7f1f9dc2d1954fcf7
Antop <antop@kakao.com>
2021-07-01 15:55:11 +0900

    코드 개선

--- src/main/kotlin/com/leetcode/P227.kt
+++ src/main/kotlin/com/leetcode/P227.kt
@@ -7,27 +7,27 @@ import java.util.*
 class P227 {
 
     fun calculate(s: String): Int {
+        val S = "$s+"
         val stack = Stack<Int>()
         var op = '+'
         var n = 0
-        for (i in s.indices) {
-            val c = s[i]
-            if (c in '0'..'9') {
-                n = (n * 10) + (c - '0')
+        loop@ for (i in S.indices) {
+            when (val c = S[i]) {
+                ' ' -> continue@loop
+                in '0'..'9' -> n = (n * 10) + (c - '0')
+                else -> {
+                    stack.push(
+                        when (op) {
+                            '-' -> n * -1
+                            '*' -> stack.pop() * n
+                            '/' -> stack.pop() / n
+                            else -> n
+                        }
+                    )
+                    n = 0
+                    op = c
+                }
             }
-            if ((c !in '0'..'9' && c != ' ') || i == s.lastIndex) {
-                stack.push(
-                    when (op) {
-                        '-' -> n * -1
-                        '*' -> stack.pop() * n
-                        '/' -> stack.pop() / n
-                        else -> n
-                    }
-                )
-                n = 0
-                op = c
-            }
-
         }
 
         return stack.sum()
