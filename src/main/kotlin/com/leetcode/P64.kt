25aa803cfb0e36053be8cbc9520027679034d1d6
Antop <antop@kakao.com>
2021-07-05 05:10:06 +0900

    algorithm-292 64. Minimum Path Sum

--- src/main/kotlin/com/leetcode/P64.kt
+++ src/main/kotlin/com/leetcode/P64.kt
@@ -3,20 +3,17 @@ package com.leetcode
 // https://leetcode.com/problems/minimum-path-sum/description/
 class P64 {
     fun minPathSum(grid: Array<IntArray>): Int {
-        val rows = grid.size
-        val cols = grid[0].size
+        val m = grid.size
+        val n = grid[0].size
 
-        for (i in 1 until cols) { // 첫번째 행
-            grid[0][i] += grid[0][i - 1]
-        }
-        for (i in 1 until rows) { // 첫번째 열
-            grid[i][0] += grid[i - 1][0]
-        }
-        for (i in 1 until rows) {
-            for (j in 1 until cols) {
+        for (i in 1 until n) grid[0][i] += grid[0][i - 1]
+        for (i in 1 until m) grid[i][0] += grid[i - 1][0]
+        for (i in 1 until m) {
+            for (j in 1 until n) {
                 grid[i][j] += minOf(grid[i - 1][j], grid[i][j - 1])
             }
         }
-        return grid[rows - 1][cols - 1]
+
+        return grid[m - 1][n - 1]
     }
 }
--- src/test/kotlin/com/leetcode/P64Test.kt
+++ src/test/kotlin/com/leetcode/P64Test.kt
@@ -4,19 +4,31 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.Test
 
-internal class P64Test {
+class P64Test {
     val p = P64()
 
     @Test
     fun `example 01`() {
         assertThat(
             p.minPathSum(
-                arrayOf(
+                grid = arrayOf(
                     intArrayOf(1, 3, 1),
                     intArrayOf(1, 5, 1),
                     intArrayOf(4, 2, 1)
                 )
             ), `is`(7)
+        )
+    }
+
+    @Test
+    fun `example 02`() {
+        assertThat(
+            p.minPathSum(
+                grid = arrayOf(
+                    intArrayOf(1, 2, 3),
+                    intArrayOf(4, 5, 6)
+                )
+            ), `is`(12)
         )
     }
 }
