6ec5480c7406763ed87367c507a3b24d0a050912
Antop <antop@kakao.com>
2021-08-23 03:23:00 +0900

    algorithm-316 79. Word Search 약간 개선

--- src/main/kotlin/com/leetcode/P79.kt
+++ src/main/kotlin/com/leetcode/P79.kt
@@ -14,16 +14,14 @@ class P79 {
     }
 
     private fun dfs(board: Array<CharArray>, i: Int, j: Int, word: String, k: Int): Boolean {
+        if (k == word.length) return true
         // 행의 인덱스가 벗어난 경우
         if (i !in board.indices) return false
         // 열의 인덱스가 벗어난 경우
         if (j !in board[0].indices) return false
         // 단어가 틀린 경우
         if (board[i][j] != word[k]) return false
-        // 미지막 글자였으면 true
-        if (k == word.lastIndex) return true
         // 방문 체크
-        val temp = board[i][j]
         board[i][j] = ' '
 
         return if (dfs(board, i - 1, j, word, k + 1) // ↑
@@ -34,7 +32,7 @@ class P79 {
             true
         } else {
             // 지나갔던 경로들이 답이 아니었다면 다시 복구한다.
-            board[i][j] = temp
+            board[i][j] = word[k]
             false
         }
     }
