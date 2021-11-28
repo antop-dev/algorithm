8272507c6aa8c074a3526e355645acff758e6914
Antop <antop@kakao.com>
2021-11-28 22:23:39 +0900

    algorithm-354 461. Hamming Distance

--- src/main/kotlin/com/leetcode/P461.kt
+++ src/main/kotlin/com/leetcode/P461.kt
@@ -3,19 +3,14 @@ package com.leetcode
 // https://github.com/antop-dev/algorithm/issues/354
 class P461 {
     fun hammingDistance(x: Int, y: Int): Int {
-        var answer = 0
-        var a = x
-        var b = y
+        var dist = 0
+        var n = x.xor(y) // x ^ y
 
-        while (a > 0 || b > 0) {
-            val c = a % 2
-            val d = b % 2
-            if (c != d) answer++
-
-            a /= 2
-            b /= 2
+        while (n > 0) {
+            dist += n.and(1)
+            n = n.shr(1) // n >> 1
         }
 
-        return answer
+        return dist
     }
 }
