e4778d681a8d9f2276fb132b258f7fde9e58453c
Antop <antop@kakao.com>
2022-01-04 00:20:11 +0900

    algorithm-364 1679. Max Number of K-Sum Pairs

---
+++ src/main/kotlin/com/leetcode/P1679.kt
@@ -0,0 +1,27 @@
+package com.leetcode
+
+// https://github.com/antop-dev/algorithm/issues/364
+class P1679 {
+    fun maxOperations(nums: IntArray, k: Int): Int {
+        nums.sort()
+
+        var answer = 0
+        var i = 0
+        var j = nums.lastIndex
+        while (i < j) {
+            val sum = nums[i] + nums[j]
+            println("i = $i, j = $j, sum = ${nums[i]} + ${nums[j]} = $sum")
+            if (sum == k) {
+                answer++
+                i++
+                j--
+            } else if (sum < k) {
+                i++
+            } else { // sum > k
+                j--
+            }
+        }
+
+        return answer
+    }
+}
