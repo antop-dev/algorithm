488c1f6004f050ff611666f872999804e318a088
Antop <antop@kakao.com>
2022-01-08 15:16:58 +0900

    코틀린 1.3.10 (LeetCode) 기준으로 파일 읽기 작성

--- src/test/kotlin/FileBase.kt
+++ src/test/kotlin/FileBase.kt
@@ -4,6 +4,7 @@ import java.nio.file.Paths
 
 abstract class FileBase {
     protected fun <T> readFile(path: String, clazz: Class<T>): T {
-        return Gson().fromJson(Files.readString(Paths.get(path)), clazz)
+//        return Gson().fromJson(Files.readString(Paths.get(path)), clazz)
+        return Gson().fromJson(Files.readAllLines(Paths.get(path))[0], clazz)
     }
 }
