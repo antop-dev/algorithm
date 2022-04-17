17d830070a99590ff43f7356caf8ef5d6271b65b
Antop <antop@kakao.com>
2022-04-17 13:42:21 +0900

    삼각 달팽이 - 다른 풀이

--- src/main/kotlin/kr/co/programmers/P68645.kt
+++ src/main/kotlin/kr/co/programmers/P68645.kt
@@ -3,46 +3,19 @@ package kr.co.programmers
 // https://github.com/antop-dev/algorithm/issues/242
 class P68645 {
     fun solution(n: Int): IntArray {
-        val len = (1..n).sum()
-        val box = Array(n) { Array(n) { 0 } }
-        var y1 = 0 // y from
-        var y2 = n - 1 // y to
-        var x1 = 0 // x from
-        var x2 = n - 1 // x to
-        // direction % 3 == 1 -> down
-        // direction % 3 == 2 -> right
-        // direction % 3 == 0 -> up left
-        var direction = 1
+        val box = Array(n) { IntArray(n) }
+        var N = n
+        var row = -1
+        var col = 0
         var num = 1
-        while (num <= len) {
-            when (direction % 3) {
-                1 -> {
-                    for (y in y1..y2) box[y][x1] = num++
-                    x1++
-                    y1++
-                }
-                2 -> {
-                    for (x in x1..x2) box[y2][x] = num++
-                    y2--
-                    x2--
-                }
-                0 -> {
-                    var y = y2
-                    var x = x2
-                    while (y >= y1) box[y--][x--] = num++
-                    y1++
-                    x2--
-                }
-            }
-            direction++
+        while (N > 0) {
+            repeat(N) { box[++row][col] = num++ }
+            repeat(N - 1) { box[row][++col] = num++ }
+            repeat(N - 2) { box[--row][--col] = num++ }
+            N -= 3
         }
-
-        val answer = Array(len) { 0 }
-        var i = 0
-        for (y in 0 until n) {
-            for (x in 0..y) answer[i++] = box[y][x]
-        }
-
-        return answer.toIntArray()
+        // 0을 제외하고 2차원 배열을 1차원 배열로 합치기
+        return box.flatMap { it.filter { n -> n != 0 } }.toIntArray()
     }
+
 }
