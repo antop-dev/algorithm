6ae2b9b048cce6184abf6e083a99cea395d385d0
안정용 <antop@hanwha.com>
2022-04-25 14:15:24 +0900

    540. Single Element in a Sorted Array - 코드 정리

--- src/main/kotlin/com/leetcode/P540.kt
+++ src/main/kotlin/com/leetcode/P540.kt
@@ -2,21 +2,24 @@ package com.leetcode
 
 // https://github.com/antop-dev/algorithm/issues/247
 class P540 {
-    fun singleNonDuplicate(nums: IntArray): Int {
-        return bs(nums, 0, nums.lastIndex)
-    }
 
-    private fun bs(nums: IntArray, l: Int, r: Int): Int {
+    fun singleNonDuplicate(nums: IntArray, l: Int = 0, r: Int = nums.lastIndex): Int {
         if (l > r) return 0
         if (l == r) return nums[l]
-        val m = (l + r) / 2
 
+        val m = (l + r) / 2
         return if (m % 2 == 0) { // 짝수 인덱스일 경우
-            if (nums[m] == nums[m + 1]) bs(nums, m + 2, r)
-            else bs(nums, l, m)
+            if (nums[m] == nums[m + 1]) {
+                singleNonDuplicate(nums, m + 2, r)
+            } else {
+                singleNonDuplicate(nums, l, m)
+            }
         } else { // 홀수 인덱스일 경우
-            if (nums[m] == nums[m - 1]) bs(nums, m + 1, r)
-            else bs(nums, l, m - 1)
+            if (nums[m] == nums[m - 1]) {
+                singleNonDuplicate(nums, m + 1, r)
+            } else {
+                singleNonDuplicate(nums, l, m - 1)
+            }
         }
     }
 
