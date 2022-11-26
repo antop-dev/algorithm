cc77c012fcedbf525d31fea929e5100b084b97c1
Antop <antop@kakao.com>
2022-11-26 17:18:29 +0900

    스택을 이용한 풀이

--- src/main/kotlin/com/leetcode/P2375.kt
+++ src/main/kotlin/com/leetcode/P2375.kt
@@ -1,28 +1,23 @@
 package com.leetcode
 
+import java.util.*
+
 // https://github.com/antop-dev/algorithm/issues/435
 class P2375 {
     fun smallestNumber(pattern: String): String {
-        val set = mutableSetOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
-        return dfs(pattern, set, 0, 0).toString()
-    }
+        var n = 1
+        val stack = Stack<Int>()
+        val sb = StringBuilder()
 
-    private fun dfs(pattern: String, set: MutableSet<Int>, num: Int, i: Int): Int {
-        if (i > pattern.length) return num
-        val prev = num % 10
-        var min = Int.MAX_VALUE
-        for (n in 1..9) {
-            if (i > 0) {
-                if (!set.contains(n)) continue
-                if (pattern[i - 1] == 'I' && n < prev) continue
-                if (pattern[i - 1] == 'D' && n > prev) continue
+        for (p in pattern + "I") {
+            stack.push(n++)
+            if (p == 'I') {
+                while (stack.isNotEmpty()) {
+                    sb.append(stack.pop())
+                }
             }
-
-            set -= n
-            val v = dfs(pattern, set, (num * 10) + n, i + 1)
-            if (v < min) min = v
-            set += n // backtracking
         }
-        return min
+        return sb.toString()
     }
+
 }
