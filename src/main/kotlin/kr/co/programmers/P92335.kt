da26a1ded336e6e6769cda35248f67e464aba720
Antop <antop@kakao.com>
2022-12-11 13:45:33 +0900

    코드 정리

--- src/main/kotlin/kr/co/programmers/P92335.kt
+++ src/main/kotlin/kr/co/programmers/P92335.kt
@@ -4,38 +4,50 @@ import java.math.BigInteger
 
 // https://github.com/antop-dev/algorithm/issues/403
 class P92335 {
+    companion object {
+        val TEEN: BigInteger = BigInteger("10")
+    }
 
     fun solution(n: Int, k: Int): Int {
-        val teen = BigInteger.valueOf(10)
+
         var answer = 0
-        // n.toString(k)
-        val kNumber = decimalToK(n, k) + "0"
+        val kNumber = decimalToK(n, k) + "0" // n.toString(k) + "0"
         var num = BigInteger.ZERO
-        for (c in kNumber) {
-            num = when {
-                (c == '0' && num != BigInteger.ZERO) -> {
-                    if (isPrime(num)) answer++
-                    BigInteger.ZERO
+        for (ch in kNumber) { // 한 문자씩 탐색
+            val n = ch - '0'
+            num = if (n == 0 && num != BigInteger.ZERO) { // 조건에 맞는 경우
+                if (isPrime(num)) { // 소수인지 판단
+                    answer++
                 }
-                else -> num.multiply(teen).add(BigInteger("${c - '0'}"))
+                BigInteger.ZERO // 수 초기화
+            } else { // 숫자를 완성해 나간다.
+                // num = (num * 10) + n
+                (num * TEEN) + BigInteger("$n")
             }
         }
         return answer
     }
 
-    private fun decimalToK(n: Int, k: Int) = StringBuffer().apply {
-        var m = n
-        while (m >= k) {
-            insert(0, m % k)
-            m /= k
+    // 10진수를 k진수로 변환
+    private fun decimalToK(n: Int, k: Int): String {
+        val sb = StringBuilder()
+        var num = n
+        while (num >= k) {
+            sb.insert(0, num % k)
+            num /= k
         }
-        insert(0, m)
-    }.toString()
+        sb.insert(0, num)
+        return sb.toString()
+    }
 
-    private fun isPrime(n: BigInteger): Boolean {
-        if (n == BigInteger.ZERO || n == BigInteger.ONE) return false
-        for (i in 2 until n.sqrt().toLong() + 1) {
-            if (n.mod(BigInteger.valueOf(i)) == BigInteger.ZERO) {
+    // 소수인지 판단
+    private fun isPrime(num: BigInteger): Boolean {
+        if (num <= BigInteger.ONE) {
+            return false
+        }
+        for (n in 2 until num.sqrt().toLong() + 1) {
+            // 나눠지면 소수가 아니다
+            if (num.mod(BigInteger("$n")) == BigInteger.ZERO) {
                 return false
             }
         }
