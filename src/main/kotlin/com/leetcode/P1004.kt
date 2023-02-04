f80aade0093967e72532d0dcd7fb1c993a92b47e
Antop <antop@kakao.com>
2023-02-04 16:06:35 +0900

    코드 변경

--- src/main/kotlin/com/leetcode/P1004.kt
+++ src/main/kotlin/com/leetcode/P1004.kt
@@ -1,12 +1,14 @@
 package com.leetcode
 
-// https://leetcode.com/problems/max-consecutive-ones-iii/
+// https://github.com/antop-dev/algorithm/issues/98
 class P1004 {
     fun longestOnes(A: IntArray, K: Int): Int {
         var result = 0
-        var zeros = 0
+        var zeros = 0 // count of zero
         var i = 0 // first pointer
-        A.forEachIndexed { j, num -> // second pointer
+        var j = 0 // second pointer
+        for (j in A.indices) {
+            val num = A[j]
             if (num == 0) zeros++
             if (zeros > K) {
                 if (A[i] == 0) zeros--
