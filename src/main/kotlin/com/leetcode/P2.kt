9324a7251603b9a7616c84cf5fe419547647c4a9
Antop <antop@kakao.com>
2023-06-07 12:25:29 +0900

    Add Two Numbers 다시 품

--- src/main/kotlin/com/leetcode/P2.kt
+++ src/main/kotlin/com/leetcode/P2.kt
@@ -1,60 +1,32 @@
 package com.leetcode
 
-/**
- * Example:
- * var li = ListNode(5)
- * var v = li.`val`
- * Definition for singly-linked list.
- * class ListNode(var `val`: Int) {
- *     var next: ListNode? = null
- * }
- */
-
-/**
- * https://leetcode.com/problems/add-two-numbers/
- */
+// https://github.com/antop-dev/algorithm/issues/33
 class P2 {
     fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-        // 코틀린에서 펑션의 인자는 상수다
-        var node1: ListNode? = l1
-        var node2: ListNode? = l2
-        // 리턴할 노드
-        var root: ListNode? = null
+        var curr = ListNode(0)
+        val root = curr
 
-        var node: ListNode? = null
-        // 두 수의 합이 10이 넘었다는 플래그
-        // 이 값이 참이면 다음 노드의 합을 계산할 +1 해야 한다
-        var over = false
-        while (node1 != null || node2 != null) {
+        var node1 = l1
+        var node2 = l2
+        var over = 0 // 일의 자리가 넘어감
+
+        while (node1 != null || node2 != null || over > 0) {
             val x = node1?.`val` ?: 0
             val y = node2?.`val` ?: 0
-            var sum = x + y + if (over) 1 else 0
+            val v = over + x + y
+            over = v / 10
 
-            over = sum >= 10
-            if (over) sum %= 10
+            curr.next = ListNode(v % 10)
+            curr = curr.next!!
 
-            val n = ListNode(sum)
-            // if (root == null) { root = n }
-            root = root ?: n
-
-            node?.next = n // 다음 연결
-            node = n // 현재 노드 교체
-
-            // 다음 노드
             node1 = node1?.next
             node2 = node2?.next
         }
 
-        if (over) {
-            // 마지막 모든 노드를 처리 후 10의자리가 넘었을 경우
-            node?.next = ListNode(1)
-        }
-
-        return root
+        return root.next
     }
 
     class ListNode(var `val`: Int) {
         var next: ListNode? = null
     }
 }
-
