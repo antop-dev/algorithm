93907ebad0ad7288d9f5c8306daf55694878915d
Antop <antop@kakao.com>
2023-07-11 15:41:33 +0900

    DP로 풀이

--- src/main/kotlin/com/leetcode/P279.kt
+++ src/main/kotlin/com/leetcode/P279.kt
@@ -1,38 +1,23 @@
 package com.leetcode
 
-import java.util.*
 import kotlin.math.sqrt
 
 // https://github.com/antop-dev/algorithm/issues/495
 class P279 {
     fun numSquares(n: Int): Int {
-        // 중복된 수를 제거하기 위한 Set
-        val set = mutableSetOf<Int>()
-        set += n
-        // 큐
-        val queue = LinkedList<Int>()
-        queue += n
-        // 계산
-        var count = 0
-        loop@ while (queue.isNotEmpty()) {
-            count++
-            println("count = $count, queue = $queue")
-            val size = queue.size
-            for (i in 0 until size) {
-                val v = queue.poll()
-                // 가능한 가장 큰 제곱수
-                val sqrt = sqrt(v.toDouble()).toInt()
-                for (x in sqrt downTo 1) {
-                    val y = v - (x * x)
-                    if (y == 0) break@loop
-                    if (y !in set) {
-                        queue += y
-                        set += y
-                    }
+        val dp = IntArray(n + 1) { Int.MAX_VALUE }
+        dp[n] = 0
+
+        for (v in dp.indices.reversed()) {
+            // 가능한 가장 큰 제곱수
+            val sqrt = sqrt(v.toDouble()).toInt()
+            for (x in sqrt downTo 1) {
+                val y = v - (x * x)
+                if (dp[v] + 1 < dp[y]) {
+                    dp[y] = dp[v] + 1
                 }
             }
         }
-        return count
+        return dp[0]
     }
-
 }
