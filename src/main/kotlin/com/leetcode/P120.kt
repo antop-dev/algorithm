68690185c9a8dc5ce3ed61c5e13c9b0b8c86e773
Antop <antop@kakao.com>
2023-09-03 21:20:19 +0900

    120. Triangle - 배열로 풀이

--- src/main/kotlin/com/leetcode/P120.kt
+++ src/main/kotlin/com/leetcode/P120.kt
@@ -3,12 +3,17 @@ package com.leetcode
 // https://github.com/antop-dev/algorithm/issues/382
 class P120 {
     fun minimumTotal(triangle: List<List<Int>>): Int {
-        val dp = triangle.map { it.toMutableList() }.toMutableList()
+        // 변경 가능한 배열 선언
+        val dp = Array(triangle.size) { i ->
+            IntArray(i + 1) { j -> triangle[i][j] }
+        }
+        // 합을 위로 올린다.
         for (i in dp.lastIndex - 1 downTo 0) {
             for (j in dp[i].indices) {
                 dp[i][j] += minOf(dp[i + 1][j], dp[i + 1][j + 1])
             }
         }
+        // 최상위(0,0)에 있는 수가 답
         return dp[0][0]
     }
 }
