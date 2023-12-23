e60e9a4f71936dc614a283c29ea6a55daa1a7824
Antop <antop@kakao.com>
2023-12-23 21:18:50 +0900

    64. 코드 정리

--- src/main/kotlin/com/leetcode/P64.kt
+++ src/main/kotlin/com/leetcode/P64.kt
@@ -3,16 +3,18 @@ package com.leetcode
 // https://github.com/antop-dev/algorithm/issues/99
 class P64 {
     fun minPathSum(grid: Array<IntArray>): Int {
-        for (i in grid.indices) {
-            for (j in grid[i].indices) {
+        val n = grid.size
+        val m = grid[0].size
+        for (i in 0 until n) {
+            for (j in 0 until m) {
                 grid[i][j] += when {
-                    (i <= 0 && j <= 0) -> 0
-                    (i <= 0) -> grid[i][j - 1]
-                    (j <= 0) -> grid[i - 1][j]
+                    (i == 0 && j == 0) -> 0 // 1행1열
+                    (i == 0) -> grid[i][j - 1] // 1행
+                    (j == 0) -> grid[i - 1][j] // 1열
                     else -> minOf(grid[i - 1][j], grid[i][j - 1])
                 }
             }
         }
-        return grid.last().last()
+        return grid[n - 1][m - 1]
     }
 }
