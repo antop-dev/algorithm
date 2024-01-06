3fb4812dc88826a2baa4146a95ead1944295f00f
Antop <antop@kakao.com>
2024-01-06 14:02:30 +0900

    algorithm-531 전통적인(?) DP 풀이

--- src/main/kotlin/kr/co/programmers/P161988.kt
+++ src/main/kotlin/kr/co/programmers/P161988.kt
@@ -2,29 +2,28 @@ package kr.co.programmers
 
 // https://github.com/antop-dev/algorithm/issues/531
 class P161988 {
+
     fun solution(sequence: IntArray): Long {
-        var ans = 0L
-        var p = -1L
-        var sum1 = 0L
-        var sum2 = 0L
-        for (v in sequence) {
-            // -1, 1, -1, 1, ...
-            if (sum1 + v * p >= 0) {
-                sum1 += v * p
-                if (sum1 > ans) ans = sum1
-            } else {
-                sum1 = 0
-            }
-            // 1, -1, 1, -1, ...
-            p *= -1
-            if (sum2 + v * p >= 0) {
-                sum2 += v * p
-                if (sum2 > ans) ans = sum2
-            } else {
-                sum2 = 0
-            }
+        val pulse1 = max(sequence, 1) // [1, -1, 1, ...]
+        val pulse2 = max(sequence, -1) // [-1, 1, -1, ...]
+        return maxOf(pulse1, pulse2)
+    }
+
+    private fun max(sequence: IntArray, start: Int): Long {
+        val n = sequence.size
+        val dp = LongArray(n)
+        var sign = start
+        for (i in 0 until n) {
+            dp[i] = sequence[i] * sign * 1L
+            sign *= -1
         }
-        return ans
+        var max = dp[0]
+        for (i in 1 until n) {
+            // dp[n] = n번째 원소를 포함했을 때의 최대값
+            dp[i] = maxOf(dp[i - 1] + dp[i], dp[i])
+            max = maxOf(max, dp[i])
+        }
+        return max
     }
 
 }
