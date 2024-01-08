1e1a0e0fc575ace884a544b6d36a4bf852aec716
Antop <antop@kakao.com>
2024-01-08 16:51:40 +0900

    [귤 고르기] 자바 → 코틀린으로 풀이

--- src/main/java/kr/co/programmers/P138476.kt
+++ src/main/java/kr/co/programmers/P138476.kt
@@ -1,33 +1,23 @@
-package kr.co.programmers;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map.Entry;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+package kr.co.programmers
 
 // https://github.com/antop-dev/algorithm/issues/437
-public class P138476 {
-    public int solution(int k, int[] tangerine) {
-        List<Entry<Integer, Long>> collect = Arrays.stream(tangerine)
-                .boxed()
-                // 사과 크기 : 사과 개수 집계
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet()
-                .stream()
-                // 사과 개수로 내림차순 정렬
-                .sorted((o1, o2) -> (int) (o2.getValue() - o1.getValue()))
-                .collect(Collectors.toList());
-        // [0] = 사과 종류 개수
-        // [1] = 사과 총 개수
-        int[] accumulate = new int[]{0, 0};
-        for (Entry<Integer, Long> e : collect) {
-            accumulate[0]++;
-            accumulate[1] += e.getValue();
-            if (accumulate[1] >= k) {
-                break;
+class P138476 {
+    fun solution(k: Int, tangerine: IntArray): Int {
+        var ans = 0 // 귤의 크기 종류 수
+        var box = 0 // 상자에 담은 귤 수
+
+        tangerine
+            .toTypedArray()
+            .groupingBy { it }
+            .eachCount() // 크기 : 개수
+            .entries.sortedByDescending { it.value }
+            .forEach { (_, v) ->
+                if (box >= k) return@forEach
+                ans++
+                box += v
             }
-        }
-        return accumulate[0];
+
+        return ans
     }
+
 }
--- src/test/java/kr/co/programmers/P138476Test.kt
+++ src/test/java/kr/co/programmers/P138476Test.kt
@@ -1,26 +1,24 @@
-package kr.co.programmers;
+package kr.co.programmers
 
-import org.junit.jupiter.api.Test;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.jupiter.api.Test
 
 class P138476Test {
-    private final P138476 sut = new P138476();
+    private val sut = P138476()
 
     @Test
-    void example01() {
-        assertThat(sut.solution(6, new int[]{1, 3, 2, 5, 4, 5, 2, 3}), is(3));
+    fun `example 1`() {
+        assertThat(sut.solution(6, intArrayOf(1, 3, 2, 5, 4, 5, 2, 3)), `is`(3))
     }
 
     @Test
-    void example02() {
-        assertThat(sut.solution(4, new int[]{1, 3, 2, 5, 4, 5, 2, 3}), is(2));
+    fun `example 2`() {
+        assertThat(sut.solution(4, intArrayOf(1, 3, 2, 5, 4, 5, 2, 3)), `is`(2))
     }
 
     @Test
-    void example03() {
-        assertThat(sut.solution(2, new int[]{1, 1, 1, 1, 2, 2, 2, 3}), is(1));
+    fun `example 3`() {
+        assertThat(sut.solution(2, intArrayOf(1, 1, 1, 1, 2, 2, 2, 3)), `is`(1))
     }
-
 }
