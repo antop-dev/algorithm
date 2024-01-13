7e3da57e12b7b82bfb180875ee6c9bd95dc8a865
Antop <antop@kakao.com>
2024-01-14 03:50:21 +0900

    [귤 고르기] 귤의 개수로만 풀이

--- src/main/java/kr/co/programmers/P138476.kt
+++ src/main/java/kr/co/programmers/P138476.kt
@@ -3,20 +3,21 @@ package kr.co.programmers
 // https://github.com/antop-dev/algorithm/issues/437
 class P138476 {
     fun solution(k: Int, tangerine: IntArray): Int {
-        var ans = 0 // 귤의 크기 종류 수
-        var box = 0 // 상자에 담은 귤 수
-
-        tangerine
+        // 개수가 많은 순서대로 정렬(내림차순)
+        val values = tangerine
             .toTypedArray()
             .groupingBy { it }
             .eachCount() // 크기 : 개수
-            .entries.sortedByDescending { it.value }
-            .forEach { (_, v) ->
-                if (box >= k) return@forEach
-                ans++
-                box += v
-            }
+            .values
+            .sortedDescending()
 
+        var ans = 0 // 귤의 크기 종류 수
+        var box = 0 // 상자에 담은 귤 수
+        for (v in values) {
+            if (box >= k) break
+            ans++
+            box += v
+        }
         return ans
     }
 
