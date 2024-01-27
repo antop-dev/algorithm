e85baa702d48eae3512afc76e4ca669b7a2a2bf3
Antop <antop@kakao.com>
2024-01-27 17:45:39 +0900

    791. 정렬을 사용하지 않는 O(n) 풀이

--- src/main/kotlin/com/leetcode/P791.kt
+++ src/main/kotlin/com/leetcode/P791.kt
@@ -2,17 +2,28 @@ package com.leetcode
 
 // https://github.com/antop-dev/algorithm/issues/534
 class P791 {
-    companion object {
-        const val MAX = 26;
-    }
 
     fun customSortString(order: String, s: String): String {
-        val sort = IntArray(26) { MAX }
-        for (i in order.indices) {
-            sort[order[i] - 'a'] = i
+        // s 문자열의 문자의 개수
+        val counter = IntArray(26)
+        for (ch in s) counter[ch - 'a']++
+
+        return buildString {
+            for (ch in order) { // 정렬에 있는 문자
+                val i = ch - 'a'
+                val count = counter[i]
+                if (count > 0) {
+                    repeat(count) { append(ch) }
+                    counter[i] = 0
+                }
+            }
+            for (i in counter.indices) { // 정렬에 없는 문자
+                val count = counter[i]
+                if (count > 0) {
+                    repeat(count) { append('a' + i) }
+                }
+            }
         }
-        return s.toCharArray()
-            .sortedBy { sort[it - 'a'] }
-            .joinToString("")
     }
+
 }
