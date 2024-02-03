1d675ab3e90549f5269e60c37f4c1f0944d48e84
Antop <antop@kakao.com>
2024-02-03 16:19:29 +0900

    algorithm-245 다시 풀어봄

--- src/main/kotlin/com/leetcode/P22.kt
+++ src/main/kotlin/com/leetcode/P22.kt
@@ -2,22 +2,29 @@ package com.leetcode
 
 // https://github.com/antop-dev/algorithm/issues/245
 class P22 {
-    val answer = mutableListOf<String>()
 
     fun generateParenthesis(n: Int): List<String> {
-        f(n, 1, 0, "(")
-        return answer
+        return mutableListOf<String>().apply {
+            dfs(this, n, 0, 0, "")
+        }
     }
 
-    private fun f(n: Int, open: Int, close: Int, s: String) {
-        println("n = $n, open = $open, close = $close, s = $s")
-        if (open == n && close == n) {
-            answer += s
+    /**
+     * @param open 사용된 열린 괄호 수
+     * @param close 사용된 닫힌 괄호 수
+     */
+    private fun dfs(ans: MutableList<String>, n: Int, open: Int, close: Int, s: String) {
+        if (open == n && close == n) { // 열린 괄호 닫힌 괄호 전부 사용됨
+            ans += s
             return
         }
-
-        if (open < n) f(n, open + 1, close, "$s(")
-        if (close < open) f(n, open, close + 1, "$s)")
+        // 위 조건이 만족하지 않다는 것은 열린 괄호나 닫힌 괄호를 추가할 수 있다는 것!
+        if (open < n) { // 열린 괄호를 추가할 수 있는 경우
+            dfs(ans, n, open + 1, close, "$s(")
+        }
+        if (close < open) { // 닫힌 괄호를 추가할 수 있는 경우
+            dfs(ans, n, open, close + 1, "$s)")
+        }
     }
 
 }
