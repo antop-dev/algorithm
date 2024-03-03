35fc039f09b104c2221d3700fcf59a29607fa4e1
Antop <antop@kakao.com>
2024-03-03 18:54:16 +0900

    343 - 코드 정리

--- src/main/kotlin/com/leetcode/P343.kt
+++ src/main/kotlin/com/leetcode/P343.kt
@@ -1,25 +1,23 @@
 package com.leetcode
 
-import kotlin.math.pow
-
 // https://github.com/antop-dev/algorithm/issues/278
 class P343 {
     fun integerBreak(n: Int): Int {
         if (n <= 3) return n - 1
+        var share = n / 3 // 몫
+        var remain = n % 3
+        when (remain) {
+            1 -> { // 나머지가 1이면 4(1 + 3)로 만든다.
+                remain += 3
+                share--
+            }
 
-        val r = n % 3
-        var d = n / 3
-        var ans = 1
-        when (r) {
-            1 -> {
-                d--
-                ans = 4
-            }
-            2 -> {
-                ans = 2
-            }
+            0 -> remain = 1
         }
-
-        return (3.0.pow(d.toDouble()) * ans).toInt()
+        // ( 3 ^ share ) * remain
+        var ans = 1
+        repeat(share) { ans *= 3 }
+        ans *= remain
+        return ans
     }
 }
