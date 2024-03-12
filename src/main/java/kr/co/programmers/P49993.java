b7b2f1395b29241ee25b221fca9c40ad0d57a580
Antop <antop@kakao.com>
2024-03-12 21:55:23 +0900

    343 - 코드 정리

--- src/main/java/kr/co/programmers/P49993.java
+++ src/main/java/kr/co/programmers/P49993.java
@@ -1,13 +1,32 @@
 package kr.co.programmers;
 
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class P49993 {
     public int solution(String skill, String[] skillTrees) {
-        String regex = "[^" + skill + "]";
-        return (int) Arrays.stream(skillTrees)
-                .map(it -> it.replaceAll(regex, ""))
-                .filter(skill::startsWith)
-                .count();
+        int ans = 0;
+        for (String skillTree : skillTrees) {
+            if (check(skillTree, skill)) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    private boolean check(String skillTree, String skill) {
+        Queue<Character> queue = new LinkedList<>();
+        for (char ch : skill.toCharArray()) {
+            queue.offer(ch);
+        }
+        for (char ch : skillTree.toCharArray()) {
+            if (!queue.contains(ch)) {
+                continue;
+            }
+            if (queue.poll() != ch) {
+                return false;
+            }
+        }
+        return true;
     }
 }
