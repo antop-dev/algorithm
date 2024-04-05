26fa12062823b3aa1b60316f2c6a4dc99dd758c4
Antop <antop@kakao.com>
2024-04-05 13:14:42 +0900

    91 - 코드 정리

--- src/main/kotlin/com/leetcode/P91.kt
+++ src/main/kotlin/com/leetcode/P91.kt
@@ -3,16 +3,21 @@ package com.leetcode
 // https://github.com/antop-dev/algorithm/issues/492
 class P91 {
     fun numDecodings(s: String): Int {
-        val sb = StringBuilder("*$s")
-
-        val dp = IntArray(sb.length)
-        dp[0] = 1
-
-        for (i in 1 until sb.length) {
-            val one = sb[i]
-            val two = sb.substring(i - 1, i + 1)
-            if (one != '0') dp[i] += dp[i - 1]
-            if (two in "10".."26") dp[i] += dp[i - 2]
+        if (s[0] == '0') return 0
+        // 계산식을 단순화하기 위해서 앞에 한자리를 더 둔다.
+        val nums = IntArray(s.length + 1).apply {
+            for (i in s.indices) this[i + 1] = s[i] - '0'
+        }
+        val dp = IntArray(s.length + 1).apply {
+            this[0] = 1
+            this[1] = 1
+        }
+        for (i in 2 until nums.size) {
+            val n = nums[i - 1] * 10 + nums[i]
+            // i번째 한자리
+            dp[i] += if (n % 10 != 0) dp[i - 1] else 0
+            // i번째 두자리
+            dp[i] += if (n in 10..26) dp[i - 2] else 0
         }
         return dp.last()
     }
