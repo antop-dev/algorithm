0ed6e98730de268c06845cb2e06b08355c426b18
Antop <antop@kakao.com>
2024-04-24 15:50:24 +0900

    LeetCode - Restore IP Addresses 코드 정리

--- src/main/kotlin/com/leetcode/P93.kt
+++ src/main/kotlin/com/leetcode/P93.kt
@@ -2,46 +2,50 @@ package com.leetcode
 
 // https://github.com/antop-dev/algorithm/issues/331
 class P93 {
-    val answer = mutableListOf<String>()
 
     fun restoreIpAddresses(s: String): List<String> {
-        if (s.length !in 4..12) return listOf()
-        dfs(s, 0, 0, StringBuffer())
-        return answer
+        if (s.length !in 4..12) {
+            return listOf()
+        }
+        val ans = mutableListOf<String>()
+        dfs(ans, s, 0, 0, StringBuilder())
+        return ans
     }
 
     /**
+     * @param ans 완성된 답이 담길 리스트
      * @param s 원본 문자열
      * @param i 문자열의 현재 인덱스
      * @param dots 사용된 쩜(".")의 개수
      * @param ip 만들어가고 있는 문자열
      */
-    fun dfs(s: String, i: Int, dots: Int, ip: StringBuffer) {
+    fun dfs(ans: MutableList<String>, s: String, i: Int, dots: Int, ip: StringBuilder) {
         // 쩜을 다 사용했지만 문자열을 다 소비 못한 경우
-        if (dots > 3 && i < s.length) {
+        if (dots == 4 && i < s.length) {
             return
         }
 
-        if (i == s.length && dots > 3) { // 완성
-            answer += ip.toString()
+        if (i == s.length && dots == 4) { // 완성
+            ans += ip.toString()
             return
         }
 
         val prevLength = ip.length // 백트래킹할 이전 문자열 길이
-        if (i > 0) ip.append(".") // 쩜이 한번 사용됨
+        if (i >= 1) { // 쩜이 한번 사용됨
+            ip.append(".")
+        }
 
         var num = 0
         // 한자리, 두자리, 세자리 계산
         val maxIndex = minOf(s.lastIndex - (3 - dots), i + 2)
         for (j in i..maxIndex) {
-            val c = s[j]
-
+            val ch = s[j]
             // IP는 0 ~ 225만 허용된다.
-            num = (num * 10) + (c - '0')
+            num = (num * 10) + (ch - '0')
             if (num > 255) break
 
-            ip.append(c)
-            dfs(s, j + 1, dots + 1, ip)
+            ip.append(ch)
+            dfs(ans, s, j + 1, dots + 1, ip)
 
             // IP의 첫자리가 0이면 두세번째 자리는 패스
             if (num == 0) break
--- src/test/kotlin/com/leetcode/P93Test.kt
+++ src/test/kotlin/com/leetcode/P93Test.kt
@@ -6,32 +6,32 @@ import org.hamcrest.Matchers.containsInAnyOrder
 import org.junit.jupiter.api.Test
 
 class P93Test {
-    val p = P93()
+    val sut = P93()
 
     @Test
     fun `example 01`() {
-        assertThat(p.restoreIpAddresses("25525511135"), containsInAnyOrder("255.255.11.135", "255.255.111.35"))
+        assertThat(sut.restoreIpAddresses("25525511135"), containsInAnyOrder("255.255.11.135", "255.255.111.35"))
     }
 
     @Test
     fun `example 02`() {
-        assertThat(p.restoreIpAddresses("0000"), containsInAnyOrder("0.0.0.0"))
+        assertThat(sut.restoreIpAddresses("0000"), containsInAnyOrder("0.0.0.0"))
     }
 
     @Test
     fun `example 03`() {
-        assertThat(p.restoreIpAddresses("1111"), containsInAnyOrder("1.1.1.1"))
+        assertThat(sut.restoreIpAddresses("1111"), containsInAnyOrder("1.1.1.1"))
     }
 
     @Test
     fun `example 04`() {
-        assertThat(p.restoreIpAddresses("010010"), containsInAnyOrder("0.10.0.10", "0.100.1.0"))
+        assertThat(sut.restoreIpAddresses("010010"), containsInAnyOrder("0.10.0.10", "0.100.1.0"))
     }
 
     @Test
     fun `example 05`() {
         assertThat(
-            p.restoreIpAddresses("101023"),
+            sut.restoreIpAddresses("101023"),
             containsInAnyOrder("1.0.10.23", "1.0.102.3", "10.1.0.23", "10.10.2.3", "101.0.2.3")
         )
     }
@@ -39,7 +39,7 @@ class P93Test {
     @Test
     fun `example 06`() {
         assertThat(
-            p.restoreIpAddresses("172162541"),
+            sut.restoreIpAddresses("172162541"),
             containsInAnyOrder(
                 "17.216.25.41",
                 "17.216.254.1",
@@ -54,7 +54,7 @@ class P93Test {
     @Test
     fun `example 07`() {
         assertThat(
-            p.restoreIpAddresses("19216811"),
+            sut.restoreIpAddresses("19216811"),
             containsInAnyOrder(
                 "1.92.168.11",
                 "19.2.168.11",
@@ -72,7 +72,7 @@ class P93Test {
     @Test
     fun `example 08`() {
         assertThat(
-            p.restoreIpAddresses("257244281111"),
+            sut.restoreIpAddresses("257244281111"),
             Matchers.empty()
         )
     }
