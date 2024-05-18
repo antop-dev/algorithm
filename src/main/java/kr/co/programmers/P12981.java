644db2897222d172b5e8692076ae89452db9426c
Antop <antop@kakao.com>
2024-05-18 18:53:25 +0900

    [프로그래머스] 영어 끝말잇기 다시 풀이

--- src/main/java/kr/co/programmers/P12981.java
+++ src/main/java/kr/co/programmers/P12981.java
@@ -1,41 +1,20 @@
 package kr.co.programmers;
 
-/**
- * https://programmers.co.kr/learn/courses/30/lessons/12981
- */
+import java.util.HashSet;
+import java.util.Set;
+
+// https://github.com/antop-dev/algorithm/issues/16
 public class P12981 {
     public int[] solution(int n, String[] words) {
-        int[] answer = new int[]{0, 0}; // 탈락자 없는 상태
-        int i = 1;
-
-        label:
-        while (i < words.length) {
-            String b = words[i - 1]; // 전 단어
-            String c = words[i]; // 현재 단어
-
-            System.out.println(String.format("%2d 번째 : %s → %s", i + 1, b, c));
-
-            // 바로 전 단어의 끝 단어와 다를 경우
-            if (b.charAt(b.length() - 1) != c.charAt(0)) {
-                break label;
-            } else {
-                // 앞에서 나온 단어를 사용했을 경우
-                for (int j = 0; j < i; j++) {
-                    if (words[j].equals(c)) {
-                        break label;
-                    }
-                }
+        Set<String> history = new HashSet<>();
+        char prev = words[0].charAt(0);
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (prev != word.charAt(0) || !history.add(word)) {
+                return new int[]{(i % n) + 1, (i / n) + 1};
             }
-
-            i++;
+            prev = word.charAt(word.length() - 1);
         }
-
-        if (i < words.length) {
-            answer[0] = (i % n) + 1; // 탈락하는 사람의 번호
-            i += 1;
-            answer[1] = (i / n) + (i % n > 0 ? 1 : 0); // 몇 번째 차례
-        }
-
-        return answer;
+        return new int[]{0, 0};
     }
 }
--- src/test/java/kr/co/programmers/P12981Test.java
+++ src/test/java/kr/co/programmers/P12981Test.java
@@ -2,32 +2,33 @@ package kr.co.programmers;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
-/**
- * https://programmers.co.kr/learn/courses/30/lessons/12981
- */
-public class P12981Test {
-    private final P12981 p = new P12981();
+class P12981Test {
+    private final P12981 sut = new P12981();
 
     @Test
-    public void example01() {
-        assertArrayEquals(new int[]{3, 3},
-                p.solution(3, new String[]{"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"})
+    void example01() {
+        assertThat(
+                sut.solution(3, new String[]{"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"}),
+                is(new int[]{3, 3})
         );
     }
 
     @Test
-    public void example02() {
-        assertArrayEquals(new int[]{0, 0},
-                p.solution(5, new String[]{"hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"})
+    void example02() {
+        assertThat(
+                sut.solution(5, new String[]{"hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"}),
+                is(new int[]{0, 0})
         );
     }
 
     @Test
-    public void example03() {
-        assertArrayEquals(new int[]{1, 3},
-                p.solution(2, new String[]{"hello", "one", "even", "never", "now", "world", "draw"})
+    void example03() {
+        assertThat(
+                sut.solution(2, new String[]{"hello", "one", "even", "never", "now", "world", "draw"}),
+                is(new int[]{1, 3})
         );
     }
 }
