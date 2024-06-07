92ec889b1737cc1edc3b51f53c059bab11699f99
Antop <antop@kakao.com>
2024-06-07 10:25:02 +0900

    다음 큰 숫자 - 비트 세기 직접 구현

--- src/main/java/kr/co/programmers/P12911.java
+++ src/main/java/kr/co/programmers/P12911.java
@@ -3,14 +3,21 @@ package kr.co.programmers;
 // https://school.programmers.co.kr/learn/courses/30/lessons/12911
 public class P12911 {
     public int solution(int n) {
-        int count = Integer.bitCount(n);
-        int next = n + 1;
-        while (next < Integer.MAX_VALUE) {
-            if (count == Integer.bitCount(next)) {
-                return next;
+        int count = countBit(n);
+        while (++n < Integer.MAX_VALUE) {
+            if (count == countBit(n)) {
+                return n;
             }
-            next++;
         }
-        return next;
+        return n;
+    }
+
+    private int countBit(int n) {
+        int count = 0;
+        while (n > 0) {
+            count += n & 1;
+            n >>>= 1;
+        }
+        return count;
     }
 }
