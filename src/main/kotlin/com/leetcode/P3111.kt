24cbab337a904df1758f1e7121259553e41876fe
Antop <antop@kakao.com>
2024-06-19 12:39:55 +0900

    [LeetCode] 3111. Minimum Rectangles to Cover Points - 정렬만을 사용해서 풀이

--- src/main/kotlin/com/leetcode/P3111.kt
+++ src/main/kotlin/com/leetcode/P3111.kt
@@ -3,11 +3,11 @@ package com.leetcode
 // https://github.com/antop-dev/algorithm/issues/564
 class P3111 {
     fun minRectanglesToCoverPoints(points: Array<IntArray>, w: Int): Int {
-        val x = points.map { (x, _) -> x }.distinct().sorted()
+        points.sortBy { it[0] }
         var count = 1
         var anchor = 0
-        for (i in x.indices) {
-            if (x[i] - x[anchor] > w) {
+        for (i in points.indices) {
+            if (points[i][0] - points[anchor][0] > w) {
                 count++
                 anchor = i
             }
