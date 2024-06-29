c1b6346f29c013ab389823cc0c42f0fb73192faf
Antop <antop@kakao.com>
2024-06-29 20:34:27 +0900

    [프로그래머스] 최댓값과 최솟값 - 코드 개선 2

--- src/main/kotlin/kr/co/programmers/P12939.kt
+++ src/main/kotlin/kr/co/programmers/P12939.kt
@@ -6,15 +6,14 @@ class P12939 {
         val sb = StringBuilder().apply {
             append(" ")
             append(s)
-            reverse()
         }
 
         var min = Int.MAX_VALUE
         var max = Int.MIN_VALUE
         var num = 0 // 만들어지는 수
         var digit = 1 // 자리수 곱샘
-        for (ch in sb) {
-            when (ch) {
+        for (i in sb.indices.reversed()) {
+            when (val ch = sb[i]) {
                 in '0'..'9' -> { // 숫자
                     num += (ch - '0') * digit
                     digit *= 10
