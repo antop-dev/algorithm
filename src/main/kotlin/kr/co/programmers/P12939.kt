02403534d43a656bfe0279ca4f2c4eef3e78dcb6
Antop <antop@kakao.com>
2024-06-29 20:19:23 +0900

    [프로그래머스] 최댓값과 최솟값 - 코드 개선

--- src/main/kotlin/kr/co/programmers/P12939.kt
+++ src/main/kotlin/kr/co/programmers/P12939.kt
@@ -3,31 +3,33 @@ package kr.co.programmers
 // https://github.com/antop-dev/algorithm/issues/390
 class P12939 {
     fun solution(s: String): String {
-        val S = " $s"
+        val sb = StringBuilder().apply {
+            append(" ")
+            append(s)
+            reverse()
+        }
+
         var min = Int.MAX_VALUE
         var max = Int.MIN_VALUE
-
         var num = 0 // 만들어지는 수
-        var m = 1 // 곱샘
-
-        for (i in S.indices.reversed()) {
-            when (val c = S[i]) {
+        var digit = 1 // 자리수 곱샘
+        for (ch in sb) {
+            when (ch) {
                 in '0'..'9' -> { // 숫자
-                    num += (c - '0') * m
-                    m *= 10
+                    num += (ch - '0') * digit
+                    digit *= 10
                 }
                 '-' -> { // 부호
                     num *= -1
                 }
                 else -> { // 공백
-                    if (num > max) max = num
-                    if (num < min) min = num
+                    min = minOf(min, num)
+                    max = maxOf(max, num)
                     num = 0
-                    m = 1
+                    digit = 1
                 }
             }
         }
-
         return "$min $max"
     }
 }
