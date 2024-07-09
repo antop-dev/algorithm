278aab25958c75d4b8940afe0a7013b64c1d0de4
Antop <antop@kakao.com>
2024-07-09 10:00:56 +0900

    [LeetCode] 36. Valid Sudoku - 코드 정리

--- src/main/kotlin/com/leetcode/P36.kt
+++ src/main/kotlin/com/leetcode/P36.kt
@@ -4,25 +4,27 @@ package com.leetcode
 class P36 {
     fun isValidSudoku(board: Array<CharArray>): Boolean {
         val n = 9
-        val rows = Array(9) { IntArray(9) }
-        val cols = Array(9) { IntArray(9) }
-        val boxes = Array(9) { IntArray(9) }
+        val rows = Array(n) { BooleanArray(n) }
+        val cols = Array(n) { BooleanArray(n) }
+        val boxes = Array(n) { BooleanArray(n) }
 
-        for (r in 0 until n) {
-            for (c in 0 until n) {
-                if (board[r][c] == '.') continue
-
-                val j = board[r][c] - '1'
-
-                if (rows[r][j] == 1) return false
-                rows[r][j] = 1
-
-                if (cols[c][j] == 1) return false
-                cols[c][j] = 1
-
-                val i = ((r / 3) * 3) + (c / 3)
-                if (boxes[i][j] == 1) return false
-                boxes[i][j] = 1
+        for (row in 0 until n) {
+            for (col in 0 until n) {
+                if (board[row][col] == '.') continue
+                // '1' -> 0
+                // '2' -> 1
+                val num = board[row][col] - '1'
+                // 가로열 중 num이 있는가?
+                if (rows[row][num]) return false
+                // 세로열 중 num이 있는가?
+                if (cols[col][num]) return false
+                // 3x3 중 num이 있는가?
+                val box = (row / 3 * 3) + (col / 3)
+                if (boxes[box][num]) return false
+                // 마킹
+                cols[col][num] = true
+                rows[row][num] = true
+                boxes[box][num] = true
             }
         }
 
