5fc55c4f267a1b43d0519401f752f21b3f1203e9
Antop <antop@kakao.com>
2024-10-19 12:50:55 +0900

    1315 - 펑션이 값을 리턴하는 방식 풀이

--- src/main/kotlin/com/leetcode/P1315.kt
+++ src/main/kotlin/com/leetcode/P1315.kt
@@ -1,27 +1,24 @@
 package com.leetcode
 
-import java.util.*
-
 // https://github.com/antop-dev/algorithm/issues/306
 class P1315 {
     fun sumEvenGrandparent(root: TreeNode?): Int {
-        if (root == null) return 0
-        val queue = LinkedList<TreeNode>().apply {
-            this += root
+        return dfs(root, null, null)
+    }
+
+    private fun dfs(node: TreeNode?, p: TreeNode?, gp: TreeNode?): Int {
+        if (node == null) {
+            return 0
         }
 
         var sum = 0
-        while (queue.isNotEmpty()) {
-            val node = queue.poll()
-            if (node.`val` % 2 == 0) { // odd
-                sum += node?.left?.left?.`val` ?: 0
-                sum += node?.left?.right?.`val` ?: 0
-                sum += node?.right?.left?.`val` ?: 0
-                sum += node?.right?.right?.`val` ?: 0
+        gp?.let {
+            if (it.`val` % 2 == 0) {
+                sum += node.`val`
             }
-            node.left?.run { queue += this }
-            node.right?.run { queue += this }
         }
+        sum += dfs(node.left, node, p)
+        sum += dfs(node.right, node, p)
 
         return sum
     }
