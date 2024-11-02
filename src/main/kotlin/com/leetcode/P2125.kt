9059827cbf0654eb07c89bc50636c56c2c3575a4
Antop <antop@kakao.com>
2024-11-02 23:38:15 +0900

    LeetCode 2125 - 다시 풀이

--- src/main/kotlin/com/leetcode/P2125.kt
+++ src/main/kotlin/com/leetcode/P2125.kt
@@ -6,14 +6,11 @@ class P2125 {
         var ans = 0
         var before = 0 // 이전 층의 머신 수
         for (floor in bank) {
-            var count = 0 // 현재 층의 머신 수
-            for (d in floor) {
-                if (d == '1') { // 머신일 경우
-                    if (before > 0) ans += before
-                    count++
-                }
-            }
+            val count = floor.count { it == '1' }
             if (count > 0) {
+                if (before > 0) {
+                    ans += before * count
+                }
                 before = count
             }
         }
