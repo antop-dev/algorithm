4949782937963ab3fc92799d636d4652a3fd2dbb
Antop <antop@kakao.com>
2024-11-03 00:15:11 +0900

    LeetCode 6 - 다시 풀이

--- src/main/kotlin/com/leetcode/P6.kt
+++ src/main/kotlin/com/leetcode/P6.kt
@@ -3,19 +3,26 @@ package com.leetcode
 // https://github.com/antop-dev/algorithm/issues/304
 class P6 {
     fun convert(s: String, numRows: Int): String {
-        if (numRows == 1) return s
+        if (numRows == 1) {
+            return s
+        }
 
         val x = numRows + (numRows - 2)
-        val rows = Array(numRows) { "" }
+        val rows = Array(numRows) { StringBuilder() }
 
         s.forEachIndexed { i, c ->
             var j = i % x
             if (j >= numRows) {
                 j = x - j
             }
-            rows[j] += "$c"
+            rows[j].append(c)
         }
 
-        return rows.joinToString("")
+        val sb = StringBuilder()
+        for (row in rows) {
+            sb.append(row)
+        }
+
+        return sb.toString()
     }
 }
