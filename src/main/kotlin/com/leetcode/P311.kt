1b0d90da4feacda0e00fe67b760fbc71317af781
Antop <antop@kakao.com>
2025-01-09 13:42:08 +0900

    algorithm-311 1293. Shortest Path in a Grid with Obstacles Elimination

--- src/main/kotlin/com/leetcode/P311.kt
+++ src/main/kotlin/com/leetcode/P311.kt
@@ -4,49 +4,56 @@ import java.util.*
 
 // https://github.com/antop-dev/algorithm/issues/311
 class P311 {
-    private val dy = intArrayOf(-1, +0, +1, +0)
-    private val dx = intArrayOf(+0, +1, +0, -1)
+    val dy = intArrayOf(-1, 0, 1, 0)
+    val dx = intArrayOf(0, 1, 0, -1)
+
     fun shortestPath(grid: Array<IntArray>, k: Int): Int {
-        val m = grid.size
-        val n = grid[0].size
-        val memo = Array(m) { Array(n) { intArrayOf(m * n, m * n) } }
-        // [y좌표, x좌표, 장애물제거수]
-        val queue = LinkedList<IntArray>()
-        queue += intArrayOf(0, 0, 0)
+        var ans = Int.MAX_VALUE
+        val n = grid.size
+        val m = grid[0].size
+        // 각 위치의 최소 스탭 수 기록
+        val memo = Array(n) { Array(m) { Int.MAX_VALUE } }
+        memo[0][0] = 0
+        // 우선순위 큐
+        val pq = PriorityQueue<Cell>(compareBy({ it.eliminate }, { it.step }))
+        pq += Cell(0, 0, 0, 0)
 
-        var move = 0 // 이동 수
-
-        loop@ while (queue.isNotEmpty()) {
-            val size = queue.size
-            repeat(size) {
-                val (y, x, eliminate) = queue.poll()
-                // 목적지 도착
-                if (y == m - 1 && x == n - 1) { // 도착
-                    return move
-                }
-                // 네방향 체크
-                dy.zip(dx) { a, b ->
-                    val ny = y + a
-                    val nx = x + b
-
-                    if (ny in 0 until m && nx in 0 until n) {
-                        val next = intArrayOf(ny, nx, eliminate)
-                        if (grid[ny][nx] == 1) { // 벽
-                            if (eliminate < k) { // 장애물 제거 가능
-                                next[2]++
-                            } else { // 장애물 제거 불가능
-                                return@zip
-                            }
-                        }
-                        memo[ny][nx][0] = move
-                        memo[ny][nx][1] = next[2]
-                        queue += next
-                    }
-                }
+        while (pq.isNotEmpty()) {
+            val curr = pq.poll()
+            // 목적지 도착
+            if (curr.y == n - 1 && curr.x == m - 1) {
+                ans = minOf(ans, curr.step)
             }
-            move++
+            // 4방향 탐색
+            for (i in dy.indices) {
+                val ny = curr.y + dy[i]
+                val nx = curr.x + dx[i]
+                // 이동할 수 없는 조건들
+                if (ny !in 0 until n || nx !in 0 until m) continue
+                if (curr.step + 1 >= memo[ny][nx]) continue
+                if (grid[ny][nx] == 1 && curr.eliminate >= k) continue
+                // 이동 가능
+                val next = Cell(
+                    y = ny,
+                    x = nx,
+                    step = curr.step + 1,
+                    eliminate = if (grid[ny][nx] == 1) curr.eliminate + 1 else curr.eliminate
+                )
+                memo[ny][nx] = next.step
+                pq += next
+            }
         }
-        return -1
+        // 도착하지 못함
+        if (ans == Int.MAX_VALUE) {
+            ans = -1
+        }
+        return ans
     }
 
+    data class Cell(
+        val y: Int,
+        val x: Int,
+        val step: Int,
+        val eliminate: Int
+    )
 }
