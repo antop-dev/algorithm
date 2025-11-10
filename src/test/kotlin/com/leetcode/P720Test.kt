ffbd415c8c01187663caaf77ffe02443991bfc3e
antop <antop@kakao.com>
2025-11-10 12:05:18 +0900

    LeetCode 720. Set을 이용한 풀이

--- src/test/kotlin/com/leetcode/P720Test.kt
+++ src/test/kotlin/com/leetcode/P720Test.kt
@@ -17,4 +17,64 @@ class P720Test {
         assertThat(p.longestWord(arrayOf("a", "banana", "app", "appl", "ap", "apply", "apple")), `is`("apple"))
     }
 
+    @Test
+    fun `example 03`() {
+        assertThat(
+            p.longestWord(
+                arrayOf(
+                    "ts",
+                    "e",
+                    "x",
+                    "pbhj",
+                    "opto",
+                    "xhigy",
+                    "erikz",
+                    "pbh",
+                    "opt",
+                    "erikzb",
+                    "eri",
+                    "erik",
+                    "xlye",
+                    "xhig",
+                    "optoj",
+                    "optoje",
+                    "xly",
+                    "pb",
+                    "xhi",
+                    "x",
+                    "o"
+                )
+            ), `is`("e")
+        )
+    }
+
+    @Test
+    fun `example 04`() {
+        assertThat(
+            p.longestWord(
+                arrayOf(
+                    "d",
+                    "do",
+                    "dog",
+                    "p",
+                    "pe",
+                    "pen",
+                    "peng",
+                    "pengu",
+                    "pengui",
+                    "penguin",
+                    "e",
+                    "el",
+                    "ele",
+                    "elep",
+                    "eleph",
+                    "elepha",
+                    "elephan",
+                    "elephant"
+                )
+            ),
+            `is`("elephant")
+        )
+    }
+
 }
