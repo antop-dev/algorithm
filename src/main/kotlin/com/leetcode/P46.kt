efe3e0bca4e8057490b853c870cf410772dcd262
antop <antop@kakao.com>
2025-11-10 12:05:43 +0900

    LeetCode 46. 코드 정리

--- src/main/kotlin/com/leetcode/P46.kt
+++ src/main/kotlin/com/leetcode/P46.kt
@@ -2,23 +2,23 @@ package com.leetcode
 
 // https://github.com/antop-dev/algorithm/issues/264
 class P46 {
-    private val answer = mutableListOf<List<Int>>()
-
     fun permute(nums: IntArray): List<List<Int>> {
-        dfs(mutableListOf(), nums.toMutableList())
-        return answer
+        val ans = mutableListOf<List<Int>>()
+        val x = mutableListOf<Int>()
+        val y = nums.toMutableList()
+        dfs(ans, x, y)
+        return ans
     }
 
-    private fun dfs(x: MutableList<Int>, y: MutableList<Int>) {
+    private fun dfs(ans: MutableList<List<Int>>, x: MutableList<Int>, y: MutableList<Int>) {
         if (y.isEmpty()) {
-            answer += x.toList()
+            ans += x.toList()
             return
         }
         repeat(y.size) {
             x += y.removeAt(0)
-            dfs(x, y)
+            dfs(ans, x, y)
             y += x.removeAt(x.lastIndex)
         }
     }
-
 }
