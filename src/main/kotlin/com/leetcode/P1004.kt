4803f1209aaf77531999e9d9912cebc2fe12e153
antop <antop@kakao.com>
2025-12-15 11:18:44 +0900

    1004 코드 정리

--- src/main/kotlin/com/leetcode/P1004.kt
+++ src/main/kotlin/com/leetcode/P1004.kt
@@ -2,20 +2,22 @@ package com.leetcode
 
 // https://github.com/antop-dev/algorithm/issues/98
 class P1004 {
-    fun longestOnes(A: IntArray, K: Int): Int {
-        var result = 0
-        var zeros = 0 // count of zero
-        var i = 0 // first pointer
-        var j = 0 // second pointer
-        for (j in A.indices) {
-            val num = A[j]
-            if (num == 0) zeros++
-            if (zeros > K) {
-                if (A[i] == 0) zeros--
+    fun longestOnes(a: IntArray, k: Int): Int {
+        var ans = 0
+        var zeros = 0
+        var i = 0
+        for (j in a.indices) {
+            if (a[j] == 0) {
+                zeros++
+            }
+            if (zeros > k) {
+                if (a[i] == 0) {
+                    zeros--
+                }
                 i++
             }
-            result = maxOf(result, j - i + 1)
+            ans = maxOf(ans, j - i + 1)
         }
-        return result
+        return ans
     }
 }
