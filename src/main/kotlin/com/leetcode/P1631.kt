9d2841a3e0ae51c8ec008746623b69a17d04c8da
antop <antop@kakao.com>
2026-01-05 09:56:33 +0900

    Refactor minimumEffortPath to use a queue instead of a priority queue for improved performance

--- src/main/kotlin/com/leetcode/P1631.kt
+++ src/main/kotlin/com/leetcode/P1631.kt
@@ -2,35 +2,36 @@ package com.leetcode
 
 import java.util.*
 import kotlin.math.abs
-import kotlin.math.max
 
 // https://github.com/antop-dev/algorithm/issues/544
 class P1631 {
-
     fun minimumEffortPath(heights: Array<IntArray>): Int {
         val n = heights.size
         val m = heights[n - 1].size
-
-        val dist = Array(n) { IntArray(m) { Int.MAX_VALUE } }
-        dist[0][0] = 0
-        // [y, x, effort]
-        val pq = PriorityQueue<IntArray> { a, b -> a[2] - b[2] }
-        pq += intArrayOf(0, 0, 0)
-
+        val minEffect = Array(n) { IntArray(m) { Int.MAX_VALUE } }
+        minEffect[0][0] = 0
+        // [y, x]
+        val queue = LinkedList<IntArray>()
+        queue += intArrayOf(0, 0)
+        // 4방향
         val dir = arrayOf(-1 to 0, 0 to 1, 1 to 0, 0 to -1)
-        while (pq.isNotEmpty()) {
-            val (y, x, effort) = pq.poll()
+        while (queue.isNotEmpty()) {
+            val (y, x) = queue.poll()
             for ((dy, dx) in dir) {
                 val ny = y + dy
                 val nx = x + dx
-                if (ny !in 0 until n || nx !in 0 until m) continue
-                val newEffort = max(effort, abs(heights[y][x] - heights[ny][nx]))
-                if (newEffort < dist[ny][nx]) {
-                    dist[ny][nx] = newEffort
-                    pq += intArrayOf(ny, nx, newEffort)
+                if (ny !in 0 until n || nx !in 0 until m) {
+                    continue
+                }
+                // 지금까지 왔던 비용과 다음 칸으로 갈 비용 중 더 큰 값을 선택
+                val newEffect = maxOf(minEffect[y][x], abs(heights[y][x] - heights[ny][nx]))
+                // 이미 기록되어 있는 곳보다 비용이 덜 들면 갱신
+                if (newEffect < minEffect[ny][nx]) {
+                    minEffect[ny][nx] = newEffect
+                    queue += intArrayOf(ny, nx)
                 }
             }
         }
-        return dist[n - 1][m - 1]
+        return minEffect[n - 1][m - 1]
     }
 }
