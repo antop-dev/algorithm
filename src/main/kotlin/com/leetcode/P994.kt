2a67a04d62d3220e005ae5a93517e96ef7735e5a
antop <antop@kakao.com>
2026-05-26 10:54:26 +0900

    BFS로 코드 재작성

--- src/main/kotlin/com/leetcode/P994.kt
+++ src/main/kotlin/com/leetcode/P994.kt
@@ -5,40 +5,47 @@ import java.util.*
 // https://github.com/antop-dev/algorithm/issues/487
 class P994 {
     fun orangesRotting(grid: Array<IntArray>): Int {
-        val queue = LinkedList<Pair<Int, Int>>()
-        val fresh = intArrayOf(0) // 함수에 값을 넘기기위해 참조로 씀
-        for (y in grid.indices) {
-            for (x in grid[y].indices) {
-                when (grid[y][x]) {
-                    1 -> fresh[0]++ // fresh 카운팅
-                    2 -> queue += y to x // rotten 위치 큐에 저장
+        var freshs = 0 // 신선한 오렌지 갯수
+        val queue: Queue<Pair<Int, Int>> = LinkedList()
+
+        val m = grid.size
+        val n = grid[m - 1].size
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                when (grid[i][j]) {
+                    1 -> freshs++
+                    2 -> queue += (i to j)
                 }
             }
         }
 
-        if (fresh[0] == 0) return 0
+        if (freshs == 0) { // 신선한 오렌지가 없는 경우
+            return 0
+        }
 
+        // BFS
+        val dx = intArrayOf(0, 1, 0, -1)
+        val dy = intArrayOf(-1, 0, 1, 0)
         var minutes = -1
         while (queue.isNotEmpty()) {
-            repeat(queue.size) {
-                val (y, x) = queue.poll()
-                go(queue, grid, fresh, y - 1, x) // 북
-                go(queue, grid, fresh, y, x + 1) // 동
-                go(queue, grid, fresh, y + 1, x) // 남
-                go(queue, grid, fresh, y, x - 1) // 서
+            val count = queue.size
+            repeat(count) {
+                val (i, j) = queue.poll()
+                dy.zip(dx) { x, y ->
+                    val ni = i + y
+                    val nj = j + x
+                    if (ni in 0 until m &&
+                        nj in 0 until n &&
+                        grid[ni][nj] == 1
+                    ) {
+                        queue += ni to nj
+                        grid[ni][nj] = 2
+                        freshs--
+                    }
+                }
             }
             minutes++
         }
-
-        return if (fresh[0] == 0) minutes else -1
+        return if (freshs == 0) minutes else -1
     }
-
-    private fun go(queue: LinkedList<Pair<Int, Int>>, grid: Array<IntArray>, fresh: IntArray, y: Int, x: Int) {
-        if (y >= 0 && y < grid.size && x >= 0 && x < grid[y].size && grid[y][x] == 1) {
-            queue += y to x
-            grid[y][x] = 2
-            fresh[0]--
-        }
-    }
-
 }
